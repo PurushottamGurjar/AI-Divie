f2e528710ecbe8525d212f666c1aa3b9b65c075c
PurushottamGurjar <purushottamnitwarangal@gmail.com>
2025-06-12 00:42:58 +0530

    Days last commit

--- src/components/ChatMain.jsx
+++ src/components/ChatMain.jsx
@@ -1,45 +1,63 @@
-import React, { useState } from 'react'
-import "./chatmain.css"
-import reactsvg from "../assets/react.svg"
-import { myIcons } from '../assets/myassets'
+import React, { useState } from "react";
+import "./chatmain.css";
+import reactsvg from "../assets/react.svg";
+import { myIcons } from "../assets/myassets";
 
 const ChatMain = () => {
-    const [isBar,setIsBar]=useState(false)
+  const [isBar, setIsBar] = useState(false);
   return (
-    <div className='chat-main'>
-        <div className="chat-sidebar-laptop" onMouseEnter={() => setIsBar(!isBar)} 
-            onMouseLeave={()=>setIsBar(!isBar)}
-            style={{ width: `${isBar?250:50}px` }}>
-                <div className="chat-sidebar-first">
-                    <img src={myIcons.expand_icon} alt="" />
-                    <img src={myIcons.plus_icon} alt="" />
-                </div>
-                <div className="chat-sidebar-second">
-                    <img src={myIcons.setting_icon} alt="" />
-                </div>
+    <div className="chat-main">
+      <div
+        className="chat-sidebar-laptop"
+        onMouseEnter={() => setIsBar(!isBar)}
+        onMouseLeave={() => setIsBar(!isBar)}
+        style={{ width: `${isBar ? 250 : 50}px` }}
+      >
+        <div className="chat-sidebar-first">
+          <img  className="chat-sidebar-eachicon chat-expand" src={myIcons.expand_icon} alt="Expand" />
+          <img  className="chat-sidebar-eachicon chat-plus"  src={myIcons.plus_icon} alt="New Chat" />
         </div>
-        <div className="chat-sidebar-tablet" onMouseEnter={() => setIsBar(!isBar)} 
-                                onClick={()=>setIsBar(!isBar)}
-                                style={{ width: `${isBar?250:50}px` }}
->
+        <div className="chat-sidebar-second">
+          <img className="chat-sidebar-eachicon"  src={myIcons.setting_icon} alt="" />
+        </div>
+      </div>
+      <div
+        className="chat-sidebar-tablet"
+        onMouseEnter={() => setIsBar(!isBar)}
+        onClick={() => setIsBar(!isBar)}
+        style={{ width: `${isBar ? 250 : 50}px` }}
+      >
+        <div className="chat-sidebar-first">
+          <img  className="chat-sidebar-eachicon chat-expand" src={myIcons.expand_icon} alt="Expand" />
+          <img  className="chat-sidebar-eachicon chat-plus"  src={myIcons.plus_icon} alt="New Chat" />
+        </div>
+        <div className="chat-sidebar-second">
+          <img className="chat-sidebar-eachicon"  src={myIcons.setting_icon} alt="" />
+        </div>
+      </div>
 
-        </div>
-
-        <div className="chat-body" >
-            <div className="chat-header">
-                <div className="ai-name" style={{marginLeft:`${isBar?250:50}px`}}>AI-Divie</div>
-                <div>
-                    <img className='chat-profile-img' src={reactsvg} alt="image" />
-                </div>
+      <div className="chat-body">
+        <div className="chat-header">
+            <div className="chat-expand-name">
+                <img  className="chat-mobile-expand" src={myIcons.expand_icon} alt="Expand" onClick={()=>setIsBar(!isBar)}/>
+          <div
+            className="ai-name"
+            style={{ marginLeft: `${isBar ? 270 : 70}px` }}
+          >
+            AI-Divie
+          </div>
             </div>
-            <div className="chat-main-content"></div>
-            <div className="chat-search-box">
-                <div className="chat-search-box-inner"></div>
-            </div>
+          <div>
+            <img className="chat-profile-img" src={reactsvg} alt="image"  />
+          </div>
         </div>
-      
+        <div className="chat-main-content"></div>
+        <div className="chat-search-box">
+          <div className="chat-search-box-inner"></div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ChatMain
+export default ChatMain;
