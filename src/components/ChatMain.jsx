d0cb8f649c66305b717470d17b3f130b0003bcec
PurushottamGurjar <purushottamnitwarangal@gmail.com>
2025-06-14 16:58:41 +0530

    need to setup the loading animation

--- src/components/ChatMain.jsx
+++ src/components/ChatMain.jsx
@@ -1,29 +1,28 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./chatmain.css";
 import reactsvg from "../assets/react.svg";
 import { myIcons } from "../assets/myassets";
-import { useContext } from "react";
-import { ChatContext } from "../context/Context";
-
-
+import runChat from "../config/Gemini";
 
 const ChatMain = () => {
+  const [isBar, setIsBar] = useState(false);
+  const [prompt, setPrompt] = useState("");
+  const [isResponse, setIsResponse] = useState(false);
+  const [apiResponse, setApiResponse] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [prevPrompts, setPrevPrompts] = useState([]);
+  const [lastPrompt, setLastPrompt] = useState("");
 
-  const {isBar,setIsBar,
-        prompt,setPrompt,
-        isResponse, setIsResponse,
-        apiResponse, setApiResponse,
-        isLoading, setIsLoading,
-        prevPrompts,setPrevPrompts,
-        recentPrompt,setRecentPrompt,
-        HandleSend
+  const HandleSend = async () => {
+    setIsResponse(true);
+    setLastPrompt(prompt);
+    setPrompt("");
+    setIsLoading(true);
+    let response = await runChat(prompt);
+    setApiResponse(response);
+    setIsLoading(false);
+  };
 
-
-  }=useContext(ChatContext);
-
-
-
-  console.log(prompt);
   return (
     <div className="chat-main">
       <div
@@ -33,15 +32,26 @@ const ChatMain = () => {
         style={{ width: `${isBar ? 250 : 50}px` }}
       >
         <div className="chat-sidebar-first">
-          <img  className="chat-sidebar-eachicon chat-expand" src={myIcons.expand_icon} alt="Expand" />
+          <img
+            className="chat-sidebar-eachicon chat-expand"
+            src={myIcons.expand_icon}
+            alt="Expand"
+          />
           <div className="chat-sidebar-newchat-container">
-             <img  className="chat-sidebar-eachicon chat-plus"  src={myIcons.plus_icon} alt="New Chat" />
-             {isBar &&  <div className="chat-sidebar-newchat-text">New chat</div>
-              }
+            <img
+              className="chat-sidebar-eachicon chat-plus"
+              src={myIcons.plus_icon}
+              alt="New Chat"
+            />
+            {isBar && <div className="chat-sidebar-newchat-text">New chat</div>}
           </div>
         </div>
         <div className="chat-sidebar-second">
-          <img className="chat-sidebar-eachicon"  src={myIcons.setting_icon} alt="" />
+          <img
+            className="chat-sidebar-eachicon"
+            src={myIcons.setting_icon}
+            alt=""
+          />
         </div>
       </div>
       <div
@@ -51,72 +61,118 @@ const ChatMain = () => {
         style={{ width: `${isBar ? 250 : 50}px` }}
       >
         <div className="chat-sidebar-first">
-          <img  className="chat-sidebar-eachicon chat-expand" src={myIcons.expand_icon} alt="Expand" />
-          <img  className="chat-sidebar-eachicon chat-plus"  src={myIcons.plus_icon} alt="New Chat" />
+          <img
+            className="chat-sidebar-eachicon chat-expand"
+            src={myIcons.expand_icon}
+            alt="Expand"
+          />
+          <img
+            className="chat-sidebar-eachicon chat-plus"
+            src={myIcons.plus_icon}
+            alt="New Chat"
+          />
         </div>
         <div className="chat-sidebar-second">
-          <img className="chat-sidebar-eachicon"  src={myIcons.setting_icon} alt="" />
+          <img
+            className="chat-sidebar-eachicon"
+            src={myIcons.setting_icon}
+            alt=""
+          />
         </div>
       </div>
 
       <div className="chat-body">
         <div className="chat-header">
-            <div className="chat-expand-name">
-                <img  className="chat-mobile-expand" src={myIcons.expand_icon} alt="Expand" onClick={()=>setIsBar(!isBar)}/>
-          <div
-            className="ai-name"
-            style={{ marginLeft: `${isBar ? 270 : 70}px` }}
-          >
-            AI-Divie
-          </div>
+          <div className="chat-expand-name">
+            <img
+              className="chat-mobile-expand"
+              src={myIcons.expand_icon}
+              alt="Expand"
+              onClick={() => setIsBar(!isBar)}
+            />
+            <div
+              className="ai-name"
+              style={{ marginLeft: `${isBar ? 270 : 70}px` }}
+            >
+              AI-Divie
             </div>
+          </div>
           <div>
-            <img className="chat-profile-img" src={reactsvg} alt="image"  />
+            <img className="chat-profile-img" src={reactsvg} alt="image" />
           </div>
         </div>
         <div className="chat-main-content">
-            {!isResponse && <div className="chat-home-heading">Hi Developer , Welcome to AI-Divie</div>}
-            {!isResponse && <div className="chat-home-subheading">Welcome to the AI- Divie . Divie stands for My Small World ( " Meri Choti si Duniya.. " )</div>}
-           
+          {!isResponse && (
+            <div className="chat-home-heading">
+              Hi Developer , Welcome to AI-Divie
+            </div>
+          )}
+          {!isResponse && (
+            <div className="chat-home-subheading">
+              Welcome to the AI- Divie . Divie stands for My Small World ( "
+              Meri Choti si Duniya.. " )
+            </div>
+          )}
 
-
-
-            {isResponse && <div className="chat-response-container">
+          {isResponse && (
+            <div className="chat-response-container">
               <div className="prompt-container">
-                <img className="response-prompt-img" src={myIcons.message_icon} alt="" />
-                <div className="response-prompt-text">Tell me more about Indian culture !!!</div>
+                <img
+                  className="response-prompt-img"
+                  src={myIcons.message_icon}
+                  alt=""
+                />
+                <div className="response-prompt-text">{lastPrompt}</div>
               </div>
               <div className="response-container">
-                <img className="gemini-response-img" src={myIcons.message_icon} alt="" />
-                <div  dangerouslySetInnerHTML={{__html:apiResponse}} className="response-text"/>
+                <img
+                  className="gemini-response-img"
+                  src={myIcons.message_icon}
+                  alt=""
+                />
+                {isLoading ?
+                  <div className="chat-loading-animation">
+                    <hr className="loading-hr"/>
+                    <hr className="loading-hr" />
+                    <hr className="loading-hr" />
+                  </div>
+                :
+                  <div
+                    dangerouslySetInnerHTML={{ __html: apiResponse }}
+                    className="response-text"
+                  />
+                }
               </div>
             </div>
-            
-            }
-            {console.log(prompt)}
-  
-            
-               
+          )}
         </div>
         <div className="chat-search-box">
           <div className="chat-search-box-inner">
             <div className="search-body-first">
-              <textarea 
-              className="chat-search-prompt"
-              type="text" 
-              value={prompt}
-              placeholder="How can i help you ?"
-              onChange={(e)=>setPrompt(e.target.value)}
-            />
+              <textarea
+                className="chat-search-prompt"
+                type="text"
+                value={prompt}
+                placeholder="How can i help you ?"
+                onChange={(e) => setPrompt(e.target.value)}
+              />
             </div>
             <div className="search-body-second">
               <div className="search-body-second-first">
-                <img src={myIcons.plus_icon} alt="" className="search-box-plus" />
-              <div className="search-box-deepsearch"> Research</div>
-              <div className="search-box-deepsearch">Reasoning</div>
+                <img
+                  src={myIcons.plus_icon}
+                  alt=""
+                  className="search-box-plus"
+                />
+                <div className="search-box-deepsearch"> Research</div>
+                <div className="search-box-deepsearch">Reasoning</div>
               </div>
-              <img src={myIcons.send_icon} alt="send" className="search-box-plus" onClick={HandleSend} />
-             
+              <img
+                src={myIcons.send_icon}
+                alt="send"
+                className="search-box-plus"
+                onClick={HandleSend}
+              />
             </div>
           </div>
         </div>
